96cee9e5791300de0a398ebf4db36dc699953bcd
ScxFiction <scxfiction@gmail.com>
2015-03-08 10:33:27 +0100

    Valid address type ID, etc...

--- simple_objects/sample_1/demo.php
+++ simple_objects/sample_1/demo.php
@@ -16,6 +16,7 @@ $address->city_name = 'Repocity';
 $address->subdivision_name = 'State';
 $address->postal_code = '12345';
 $address->country_name = 'Imagiery';
+$address->address_type_id = 1;
 // var_export => Outputs or returns a parsable string representation of a variable. 
 echo '<tt><pre>' . var_export($address, TRUE) . '</pre></tt>'; 
 
@@ -44,6 +45,13 @@ echo $address_2;
 echo "<h2>Displaying address types...</h2>";
 echo '<tt><pre>' . var_export(Address::$valid_address_types, TRUE) . '</pre></tt>';
 
+echo '<h2>Testing address type ID validation</h2>';
+for ($id= 0; $id <= 4 ; $id++) { 
+	echo "<div>$id: ";
+	echo Address::isValidAddressTypeId($id) ? 'Valid' : 'Invalid';
+	echo "</div>";
+
+}
 
 
 
